9dc1b5ed77f0abd9fe5591a542a2b3125e041818
Julian Slatlem <julianahslatlem@gmail.com>
2022-04-27 02:12:18 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,7 +3,9 @@ import math
 print("x = unknown value\n")
 a = input("a: ")
 b = input("b: ")
+b = int(b)
 c = input("c: ")
+c = int(c)
 
 if a is "x":
-    print(math.sqrt((c * c) - (b * b)))
+    print(math.sqrt(c^2 - b^2)
