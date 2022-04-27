199f215bc424903299da53c90b62ccb13d440493
Julian Slatlem <julianahslatlem@gmail.com>
2022-04-27 02:38:52 +0200

    Ok

---
+++ calculator.py
@@ -0,0 +1,31 @@
+from itertools import dropwhile
+import math
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+print("x = unknown value\n")
+pv = int(input("Precision value: "))
+
+a = input("a: ")
+if isfloat(a):
+    a = float(a)
+    
+b = input("b: ")
+if isfloat(b):
+    b = float(b)
+    
+c = input("c: ")
+if isfloat(c):
+    c = float(c)
+    
+if a is "x":
+    print(round(math.sqrt((c * c) - (b * b)), pv))
+elif b is "x":
+    print(round(math.sqrt((c * c) - (a * a)), pv))
+elif c is "x":
+    print(round(math.sqrt((a * a) + (b * b)), pv))
