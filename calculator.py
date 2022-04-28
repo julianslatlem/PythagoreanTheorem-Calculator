40d121f36f43a1e8999e2b2e80f4554aefc352f9
Julian Slatlem <79726826+julianslatlem@users.noreply.github.com>
2022-04-29 00:34:41 +0200

    Bug Fixes.

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 import math
+import log
+
+errors=["69420"]
 
 def isfloat(num):
     try:
@@ -8,25 +11,72 @@ def isfloat(num):
         return False
 
 print("x = unknown value\n")
-pv = 2
+pv=2
 
-a = input("a: ")
-if isfloat(a):
-    a = float(a)
+currentX=0
+x=0
+side=0
+doInput=True
+while doInput:
     
-b = input("b: ")
-if isfloat(b):
-    b = float(b)
+    while side==0:
+        a=input("a: ")
+        if isfloat(a):
+            a=float(a)
+            side+=1
+            currentX=f"a"
+        elif a=="x":
+            if x!=1:
+                side+=1
+                x+=1
+            else:
+                log.warning('"'+"x"+'"'+" variable already exists.")
+        else:
+            log.error(f'Invalid input "{a}".', errors[0])
     
-c = input("c: ")
-if isfloat(c):
-    c = float(c)
+    while side==1:
+        b=input("b: ")
+        if isfloat(b):
+            b=float(b)
+            side+=1
+            currentX=f"b"
+        elif b=="x":
+            if x!=1:
+                side+=1
+                x+=1
+            else:
+                log.warning('"'+"x"+'"'+" variable already exists.")
+        else:
+            log.error(f'Invalid input "{b}".', errors[0])
     
-if a == "x":
-    print(round(math.sqrt((c * c) - (b * b)), pv))
-elif b == "x":
-    print(round(math.sqrt((c * c) - (a * a)), pv))
-elif c == "x":
-    print(round(math.sqrt((a * a) + (b * b)), pv))
+    while side==2:
+        c=input("c: ")
+        if isfloat(c):
+            c=float(c)
+            if isfloat(a) and c<=a or isfloat(b) and c<=b:
+                log.warning("Value must be bigger than "+'"'+f"{currentX}"+'"')
+            else:
+                side+=1
+        elif c=="x":
+            if x!=1:
+                side+=1
+                x+=1
+            else:
+                log.warning('"'+"x"+'"'+" variable already exists.")
+        else:
+            log.error(f'Invalid input "{c}".', errors[0])
     
-input("Press enter to continue.")
+    if x==1:
+        doInput=False
+    else:
+        side=0
+        x=0
+    
+if a=="x":
+    print(round(math.sqrt((c*c)-(b*b)),pv))
+elif b=="x":
+    print(round(math.sqrt((c*c)-(a*a)),pv))
+elif c=="x":
+    print(round(math.sqrt((a*a)+(b*b)),pv))
+
+log.misc("Press enter to exit.")
